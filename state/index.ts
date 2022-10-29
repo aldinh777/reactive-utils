74980b787783d0508041d8e0454f48dd6c944503
aldinh777 <aldi.nh789@gmail.com>
2022-10-29 21:22:23 +0800

    add additional states

--- state/index.ts
+++ state/index.ts
@@ -57,3 +57,23 @@ export function stateObserveAll<T, U>(
     observeAll(states, (values) => o.setValue(handler(values)));
     return o;
 }
+
+export function stateToggle(initial: boolean) {
+    const st = state(initial);
+    const open = () => st.setValue(true);
+    const close = () => st.setValue(false);
+    const toggle = () => st.setValue(!st.getValue());
+    return [st, open, close, toggle];
+}
+
+export function stateLocalStorage(key: string, initial: string) {
+    const st = state(initial);
+    const local = localStorage.getItem(key);
+    if (local) {
+        st.setValue(local);
+    } else {
+        localStorage.setItem(key, st.getValue());
+    }
+    st.onChange((value) => localStorage.setItem(key, value));
+    return st;
+}
