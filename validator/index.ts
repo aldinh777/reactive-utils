3d0ab9efaee159a6937b786082a7f1484b816cf6
aldinh777 <aldi.nh789@gmail.com>
2023-02-16 18:46:59 +0800

    make validator argument type unknown

--- validator/index.ts
+++ validator/index.ts
@@ -7,18 +7,18 @@ import {
     StateMap
 } from '@aldinh777/reactive/collection';
 
-export const isState = <T>(item: object): item is State<T> =>
+export const isState = <T>(item: unknown): item is State<T> =>
     has(item, 'addListener', 'onChange', 'getValue', 'setValue');
 
-export const isCollection = <K, V, R>(item: object): item is StateCollection<K, V, R> =>
+export const isCollection = <K, V, R>(item: unknown): item is StateCollection<K, V, R> =>
     has(item, 'raw', 'get', 'trigger', 'onUpdate', 'onInsert', 'onDelete');
 
 export const isMutable = <K, V, R>(
     item: StateCollection<K, V, R>
 ): item is MutableStateCollection<K, V, R> => has(item, 'set');
 
-export const isList = <T>(item: object): item is StateList<T> =>
+export const isList = <T>(item: unknown): item is StateList<T> =>
     isCollection(item) && item.raw instanceof Array;
 
-export const isMap = <T>(item: object): item is StateMap<T> =>
+export const isMap = <T>(item: unknown): item is StateMap<T> =>
     isCollection(item) && item.raw instanceof Map;
